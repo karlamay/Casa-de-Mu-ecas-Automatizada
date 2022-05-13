16def3c9275af4f7a5d75fbbfff0b4a012388367
karlamay <karlamayv@outlook.com>
2022-05-12 20:31:54 -0500

    v2.0 12May22

--- webserver.js
+++ webserver.js
@@ -9,29 +9,9 @@ var LED = new Gpio(4,'out'),
     LED5 = new Gpio(10,'out'),
     LED6 = new Gpio(9,'out'),
     LED7 = new Gpio(11,'out');
-var MOTO = [new Gpio(18,'out'),
-            new Gpio(23,'out'),
-            new Gpio(24,'out'),
-            new Gpio(25,'out')];
-var sec1 = [ //Derecha
-    [1,0,0,0],
-    [0,1,0,0],
-    [0,0,1,0],
-    [0,0,0,1]
-    ];
-var sec2 = [ //Izquierda
-    [0,0,0,1],
-    [0,0,1,0],
-    [0,1,0,0],
-    [1,0,0,0]
-    ];
-var apag = [
-    [0,0,0,0],
-    [0,0,0,0],
-    [0,0,0,0],
-    [0,0,0,0]
-    ];
+
 http.listen(1919);
+
 function handler(req,res){
     fs.readFile(__dirname + '/public/index.html',function(err,data){
         if(err){
@@ -43,3 +23,70 @@ function handler(req,res){
         return res.end();
     });
 }
+
+io.sockets.on('connection', function (socket) {
+    socket.on('domo', function(data) {
+      switch(data){
+        case '0000':
+          LED.writeSync(0);
+          break;
+        case '0001':
+          LED.writeSync(1);
+          break;
+        case '0010':
+          LED2.writeSync(0);
+          break;
+        case '0011':
+          LED2.writeSync(1);
+          break;
+        case '0100':
+          LED3.writeSync(0);
+          break;
+        case '0101':
+          LED3.writeSync(1);
+          break;
+        case '0110':
+          LED4.writeSync(0);
+          break;
+        case '0111':
+          LED4.writeSync(1);
+          break;
+        case '1000':
+          LED5.writeSync(0);
+          break;
+        case '1001':
+          LED5.writeSync(1);
+          break;
+        case '1010':
+          LED6.writeSync(0);
+          break;
+        case '1011':
+          LED6.writeSync(1);
+          break;
+        case '1100':
+          LED7.writeSync(0);
+          break;
+        case '1101':
+          LED7.writeSync(1);
+          break;
+      }
+    });
+  });
+  
+  process.on('SIGINT', function () {
+    LED.writeSync(0);
+    LED.unexport();
+    LED2.writeSync(0);
+    LED2.unexport();
+    LED3.writeSync(0);
+    LED3.unexport();
+    LED4.writeSync(0);
+    LED4.unexport();
+    LED5.writeSync(0);
+    LED5.unexport();
+    LED6.writeSync(0);
+    LED6.unexport();
+    LED7.writeSync(0);
+    LED7.unexport();
+    process.exit(); 
+});
