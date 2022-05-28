4e194647c6c22c6f6bc73788f64bebf6a1b9911c
karlamay <karlamayv@outlook.com>
2022-05-28 12:40:35 -0500

    v2.0

--- webserver.js
+++ webserver.js
@@ -24,69 +24,77 @@ function handler(req,res){
     });
 }
 
+LED.writeSync(1);
+LED2.writeSync(1);
+LED3.writeSync(1);
+LED4.writeSync(1);
+LED5.writeSync(1);
+LED6.writeSync(1);
+LED7.writeSync(1);
+
 io.sockets.on('connection', function (socket) {
     socket.on('domo', function(data) {
       switch(data){
         case '0000':
-          LED.writeSync(1);
-          break;
-        case '0001':
           LED.writeSync(0);
           break;
-        case '0010':
-          LED2.writeSync(1);
+        case '0001':
+          LED.writeSync(1);
           break;
-        case '0011':
+        case '0010':
           LED2.writeSync(0);
           break;
-        case '0100':
-          LED3.writeSync(1);
+        case '0011':
+          LED2.writeSync(1);
           break;
-        case '0101':
+        case '0100':
           LED3.writeSync(0);
           break;
-        case '0110':
-          LED4.writeSync(1);
+        case '0101':
+          LED3.writeSync(1);
           break;
-        case '0111':
+        case '0110':
           LED4.writeSync(0);
           break;
-        case '1000':
-          LED5.writeSync(1);
+        case '0111':
+          LED4.writeSync(1);
           break;
-        case '1001':
+        case '1000':
           LED5.writeSync(0);
           break;
-        case '1010':
-          LED6.writeSync(1);
+        case '1001':
+          LED5.writeSync(1);
           break;
-        case '1011':
+        case '1010':
           LED6.writeSync(0);
           break;
+        case '1011':
+          LED6.writeSync(1);
+          break;
         case '1100':
-          LED7.writeSync(1);
+          LED7.writeSync(0);
           break;
         case '1101':
-          LED7.writeSync(0);
+          LED7.writeSync(1);
           break;
       }
     });
   });
   
   process.on('SIGINT', function () {
-    LED.writeSync(0);
+    LED.writeSync(1);
     LED.unexport();
-    LED2.writeSync(0);
+    LED2.writeSync(1);
     LED2.unexport();
-    LED3.writeSync(0);
+    LED3.writeSync(1);
     LED3.unexport();
-    LED4.writeSync(0);
+    LED4.writeSync(1);
     LED4.unexport();
-    LED5.writeSync(0);
+    LED5.writeSync(1);
     LED5.unexport();
-    LED6.writeSync(0);
+    LED6.writeSync(1);
     LED6.unexport();
-    LED7.writeSync(0);
+    LED7.writeSync(1);
     LED7.unexport();
     process.exit();
 });
